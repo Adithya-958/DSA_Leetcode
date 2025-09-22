37d6f65695ea6c6a76b19b014bbff48ead5f82eb
RUDRANGI ADITHYA <83584808+Adithya-958@users.noreply.github.com>
2025-09-22 20:36:01 +0530

    Time: 4 ms (4.26%) | Memory: 9.7 MB (9.57%) - LeetSync

---
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.c
@@ -0,0 +1,31 @@
+int maxFrequencyElements(int* nums, int numsSize) {
+    int s[10001] = {0};
+    int n = numsSize;
+    for(int i =0; i < n; i++){
+            s[nums[i]]++;
+    }
+    int maxvalue = 0;
+    for(int i = 0; i < n; i++){
+        if(maxvalue < nums[i])  maxvalue = nums[i];
+    }
+    printf("%d\t", maxvalue);
+    printf("s array (index: frequency):\n");
+    for(int i = 0; i <= maxvalue; i++){  // Showing first 10 elements
+        if(s[i] > 0){
+            printf("s[%d] = %d\n", i, s[i]);
+        }
+    }
+    int max= 0;
+    for(int i = 0; i <= maxvalue; i++){  // Showing first 10 elements
+        if(max < s[i]){
+            max =s[i];
+        }
+    }    printf("%d\t\n", max);
+    int sum = 0;
+    for(int i = 0; i <= maxvalue; i++){  // Showing first 10 elements
+        if(max == s[i]){
+            sum += max; //s[i];
+        }
+    }printf("sum is %d\t ", sum);
+    return sum;
+}
