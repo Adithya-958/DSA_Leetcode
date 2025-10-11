d16e666199ab0ba76aae2fd2c82eb1b611d8fc80
RUDRANGI ADITHYA <83584808+Adithya-958@users.noreply.github.com>
2025-10-11 11:06:49 +0530

    Time: 151 ms (85.71%) | Memory: 17.9 MB (100.00%) - LeetSync

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.c
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.c
@@ -2,7 +2,6 @@ int maximumEnergy(int* a, int n, int k) {
     int dp[n];
     int max = INT_MIN;
     for(int i = n-1; i >= 0; i--){
-        int sum;
         if(i+k >= n){
             dp[i] = a[i];
         }
