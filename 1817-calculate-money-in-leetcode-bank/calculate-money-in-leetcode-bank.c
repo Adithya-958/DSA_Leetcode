5b8d4996990a67f63d144b1471f6af864f8ff2d7
RUDRANGI ADITHYA <83584808+Adithya-958@users.noreply.github.com>
2025-10-27 19:31:10 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (34.49%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.c
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.c
@@ -1,11 +1,20 @@
 int totalMoney(int n) {
-    int a = 1, res = 0;
-    for(int i = 1; i <= n; i++){
-        res += a;
-        a++;
-        if(i%7 == 0){
-            a = i/7 + 1;
-        }
+    // int a = 1, res = 0;
+    // for(int i = 1; i <= n; i++){
+    //     res += a;
+    //     a++;
+    //     if(i%7 == 0){
+    //         a = i/7 + 1;
+    //     }
+    // }
+    // return res;
+    int sum=0, res=0 ,x = n/7, y = n%7;
+    int Fst_w = 28;
+    int Lst_w = 28+(x-1)*7;
+    sum = x*(Fst_w + Lst_w)/2;
+    int start = x + 1;
+    for(int i = 0; i < y; i++){
+        res += start+i;
     }
-    return res;
+    return res+sum;
 }
