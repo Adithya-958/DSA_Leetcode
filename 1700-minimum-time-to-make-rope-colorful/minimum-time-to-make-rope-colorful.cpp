0410dd5031e4e607f6af5f2aaf79d31fa7aa30b5
RUDRANGI ADITHYA <83584808+Adithya-958@users.noreply.github.com>
2025-11-03 23:01:57 +0530

    Time: 0 ms (100.00%) | Memory: 99.7 MB (98.07%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
     int minCost(string s, vector<int>& t) {
-        int i = 1,ans = 0;
-        while(s[i] != '\0'){
-            if(s[i] == s[i-1]){
-                ans += fmin(t[i],t[i-1]);
-                t[i] = fmax(t[i],t[i-1]);
+        int ans = 0;
+        // while(s[i] != '\0'){
+        //     if(s[i] == s[i-1]){
+        //         ans += fmin(t[i],t[i-1]);
+        //         t[i] = fmax(t[i],t[i-1]);
+        //     }
+        //     i++;}
+        int l = 0;
+        for(int i = 1; i < s.size(); i++){
+            if(s[l] == s[i]){
+                (t[l]<t[i])?((ans+=t[l])?l = i : 0) : (ans+=t[i]);
             }
-            i++;
-        }return ans;
+            else{
+                l = i;   
+            }
+        }
+        return ans;
     }
 };
