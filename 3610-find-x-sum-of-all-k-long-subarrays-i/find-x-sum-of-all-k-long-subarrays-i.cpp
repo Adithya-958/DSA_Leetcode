5e32d00cac062c2e04a91db1978f79ec14a5af41
RUDRANGI ADITHYA <83584808+Adithya-958@users.noreply.github.com>
2025-11-05 10:09:14 +0530

    Time: 19 ms (43.41%) | Memory: 36.6 MB (34.73%) - LeetSync

--- 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.cpp
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
-    typedef pair<int, int> p;
+typedef pair<int,int>p;
     vector<int> findXSum(vector<int>& a, int k, int x) {
         int n = a.size();
-        vector<int> res(n - k + 1);
+        vector<int>res(n-k+1);
         
-        for(int i = 0; i < n - k + 1; i++) {
+        //priority_queue<p, vector<p>, greater<p>> pq;
+        
+        for(int i = 0; i < n-k+1; i++){
             unordered_map<int, int> freq;
-            // Min-heap to store (frequency, value) pairs with custom comparator
-      
-            priority_queue<p, vector<p>, greater<p>> pq;
-            
-            // Count frequencies in current window
-            for(int j = i; j <= i + k - 1; j++) {
+            for(int j = i; j <= i+k-1; j++){
                 freq[a[j]]++;
             }
-            
-            // Push (frequency, value) pairs to min-heap
+            priority_queue<p, vector<p>, greater<p>> pq;
             for(auto& it : freq) {
-                pq.push({it.second, it.first});  // (frequency, value)
-                if(pq.size() > x) {
-                    pq.pop();  // Remove the smallest (by frequency, then by value)
+                pq.push({it.second, it.first});
+                if(pq.size()>x){
+                    pq.pop();
                 }
             }
-            
-            // Calculate sum of all occurrences of top x frequent elements
-            int sum = 0;
-            while(!pq.empty()) {
-                auto top = pq.top();
+            int sum = 0,count = 0;
+            while(!pq.empty()){
+                auto it = pq.top();
                 pq.pop();
-                sum += top.first * top.second;  // frequency * value
+                sum += it.first * it.second;
+                
             }
             res[i] = sum;
         }
