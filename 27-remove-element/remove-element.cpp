a745cc720900b978c8c56b0b870e30ccd67331a6
Adithya Rudrangi <83584808+Adithya-958@users.noreply.github.com>
2025-12-31 19:14:25 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (81.22%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -2,11 +2,18 @@ class Solution {
 public:
     int removeElement(vector<int>& a, int val) {
         int i = 0;
-        for(int j = 0; j < a.size(); j++){
-            if(val != a[j]){
-                a[i++] = a[j];
+        int j = a.size() - 1;
+        
+        while(i <= j) {
+            if(a[i] == val) {
+                // Swap with element at j (if it's not val)
+                // Or just overwrite
+                a[i] = a[j];
+                j--;
+            } else {
+                i++;
             }
         }
-        return i;        
+        return i;  // i is the new length
     }
 };
