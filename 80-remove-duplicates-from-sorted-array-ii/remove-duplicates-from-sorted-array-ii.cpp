2c31738e0c454b99e986010012e925367300681f
Adithya Rudrangi <83584808+Adithya-958@users.noreply.github.com>
2026-01-02 00:17:37 +0530

    Time: 7 ms (73.29%) | Memory: 19.6 MB (57.08%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -4,19 +4,13 @@ public:
         int n = a.size();
         int x = 0, k = 0;
         for(int i = 0; i < n; i++){
-            int count = 0;
-            
-            for(int j = i+1; j < n; j++){
-                if(a[i] == a[j]){
-                    count++;
-                }
-                else break;
+            if(i < n-2 && a[i] == a[i+2]){
+                continue;
             }
-            a[k++] = a[i];
-            if(count >= 1){
+            else{
                 a[k++] = a[i];
             }
-            i += count;
-        }return k;
+        }
+        return k;
     }
 };
