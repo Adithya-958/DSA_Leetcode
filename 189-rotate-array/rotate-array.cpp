32b31d2ce9f952e60b402e9df9613fdbd782cd5e
Adithya Rudrangi <83584808+Adithya-958@users.noreply.github.com>
2026-01-05 23:42:37 +0530

    Time: 3 ms (9.46%) | Memory: 29.6 MB (63.94%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
+    void rev(vector<int>& a, int left, int right){
+        while(left < right){
+            a[left] = a[left]+a[right];
+            a[right] = a[left]-a[right];
+            a[left] = a[left] - a[right];
+            left++;
+            right--;
+        }
+    }
     void rotate(vector<int>& a, int k) {
         int n = a.size();
-
-        k = k%n;
-        if(k == 0)  return;
-        vector<int> res(n);
-        int j = 0;
-        for(int i = n-k; i < n; i++){
-            res[j] = a[i];cout<<res[i];
-j++;
-        }
-        //cout<<"K is"<<k<<"   j is "<<j;
-        for(int i = 0; i < n-k; i++){
-            res[j] = a[i];cout<<res[i];
-        j++;
-        }
-
-        for(int i = 0; i < n; i++){
-            
-            a[i] = res[i];
-        }
+        k=k%n;
+        if(k==0) return;
+        rev(a,0,n-1);
+        rev(a,0,k-1);
+        rev(a,k,n-1);
     }
 };
 // [1,2,1] k = 4
