6925e8f3d99d8239f007ff91887e9bf12e4b2877
Adithya Rudrangi <83584808+Adithya-958@users.noreply.github.com>
2026-03-09 15:18:38 +0530

    Time: 46 ms (51.31%) | Memory: 19.3 MB (86.17%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,10 +1,11 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        s3 = [] # list with size m+n 
-        for i,j in zip(word1,word2): #combine 2 str into 1
-            s3.append(i+j)
-        # add the remaining elements to the s3.
-        s3.append(word1[len(word2):])# word1 is big size  
-        s3.append(word2[len(word1):])
-        
-        return "".join(s3)
+        res = []
+        for i in range(max(len(word1),len(word2))):
+            if(i < len(word1)):
+                res.append(word1[i])
+            if(i < len(word2)):
+                res.append(word2[i])
+        return "".join(res)
+            
+
