987904cf0d56a4b754277918887bfc17ee5ef80a
Adithya Rudrangi <83584808+Adithya-958@users.noreply.github.com>
2026-03-16 20:36:34 +0530

    Time: 9920 ms (5.07%) | Memory: 20.3 MB (93.78%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.py
@@ -0,0 +1,23 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        # for i in range(len(nums)): # 1 7 3 6 5 6
+        #     pre,suff = 0, 0
+        #     for j in range(i):
+        #         pre += nums[j] # 1 8 
+        #     for j in range(i+1,len(nums)):
+        #         suff += nums[j] # 6 11
+        #     print(pre)
+        #     if(pre == suff):
+        #         return i
+        # return -1
+        left, total = 0,0
+        for i in range(len(nums)):
+            total += nums[i]
+        for i in range(len(nums)):
+            j,left = 0,0
+            while(j < i):
+                left += nums[j]
+                j += 1
+            if(left == (total - left- nums[i])):
+                return i
+        return -1
