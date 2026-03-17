f8375985ee5b3b8638e88b8e003cecf7dff96682
Adithya Rudrangi <83584808+Adithya-958@users.noreply.github.com>
2026-03-17 22:54:45 +0530

    Time: 7 ms (69.58%) | Memory: 19.5 MB (81.80%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.py
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.py
@@ -11,39 +11,34 @@ class Solution:
                 b1.append(nums1[i])
         return b1
     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
-        # res1 = self.isin(nums1, nums2)
-        # res2 = self.isin(nums2,nums1)
-        # return [res1,res2]
-        # set1 = set(nums1)
-        # set2 = set(nums2)
-        # answer = [[],[]]
-        # for i in set1:
-        #     if i not in set2:
-        #         answer[0].append(i)
-        # for i in set2:
-        #     if i not in set1:
-        #         answer[1].append(i)
-        # return answer 
- # m*n, m+n
- # m+n, m+n
-        k = set()
-        p = set()
-        for i in range(len(nums1)):
-            if nums1[i] not in nums2:
-                    k.add(nums1[i])
-        for i in range(len(nums2)):
-            if nums2[i] not in nums1:
-                    p.add(nums2[i])
-        return [list(k),list(p)] #m*n
-        k = []
-        p = []
-        for i in range(len(nums1)):
-            if nums1[i] not in nums2:
-                if nums1[i] not in k:
-                    k.append(nums1[i])
-        for i in range(len(nums2)):
-            if nums2[i] not in nums1:
-                if nums2[i] not in p:
-                    p.append(nums2[i])
-        #return [k,p] # m*n*k m+n
+        s1 = set(nums1)
+        s2 = set(nums2)
+        res = [[],[]]
+        for i in (s1):
+            if i not in s2:
+                res[0].append(i)
+        for i in ((s2)):
+            if i not in s1:
+                res[1].append(i)
+        return res
+        # k = set()
+        # p = set()
+        # for i in range(len(nums1)):
+        #     if nums1[i] not in nums2:
+        #             k.add(nums1[i])
+        # for i in range(len(nums2)):
+        #     if nums2[i] not in nums1:
+        #             p.add(nums2[i])
+        # return [list(k),list(p)]  # m*n m+n
+        # k = []
+        # p = []
+        # for i in range(len(nums1)):
+        #     if nums1[i] not in nums2:
+        #         if nums1[i] not in k:
+        #             k.append(nums1[i])
+        # for i in range(len(nums2)):
+        #     if nums2[i] not in nums1:
+        #         if nums2[i] not in p:
+        #             p.append(nums2[i])
+        # #return [k,p] # m*n*k m+n
         
