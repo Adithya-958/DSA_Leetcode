0c2ab38b72d8c951833ff389c3b2b9a3259f22a1
Adithya Rudrangi <83584808+Adithya-958@users.noreply.github.com>
2026-04-07 13:46:22 +0530

    Time: 6 ms (65.05%) | Memory: 20.9 MB (15.50%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.py
+++ 437-path-sum-iii/path-sum-iii.py
@@ -1,35 +1,23 @@
-class Solution(object):
-    def pathSum(self, root, target):
-        """
-        :type root: TreeNode
-        :type sum: int
-        :rtype: int
-        """
-        # define global return var
-        self.numOfPaths = 0
-        # 1st layer DFS to go through each node
-        self.dfs(root, target)
-        # return result
-        return self.numOfPaths
-    
-    # define: traverse through the tree, at each treenode, call another DFS to test if a path sum include the answer
-    def dfs(self, node, target):
-        # exit condition
-        if node is None:
-            return 
-        # dfs break down 
-        self.test(node, target) # you can move the line to any order, here is pre-order
-        self.dfs(node.left, target)
-        self.dfs(node.right, target)
-        
-    # define: for a given node, DFS to find any path that sum == target, if find self.numOfPaths += 1
-    def test(self, node, target):
-        # exit condition
-        if node is None:
-            return
-        if node.val == target:
-            self.numOfPaths += 1
-            
-        # test break down
-        self.test(node.left, target-node.val)
-        self.test(node.right, target-node.val)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import defaultdict
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        self.path = 0
+        self.ddict = defaultdict(int)
+        self.ddict[0] = 1
+        def dfs(root, currSum):
+            if not root:
+                return
+            currSum += root.val
+            self.path += self.ddict[currSum - targetSum]
+            self.ddict[currSum] += 1
+            dfs(root.left, currSum)
+            dfs(root.right, currSum)
+            self.ddict[currSum] -= 1
+        dfs(root, 0)
+        return self.path
